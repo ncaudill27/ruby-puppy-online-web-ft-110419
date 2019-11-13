5cd72b26c01e4da660dae35f55ab09278805fdf8
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-13 00:30:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
-# Add your code here
+require 'pry'
+
+class Dog
+  
+  @@all = []
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all = []
+  end
+  
+  def self.print_all
+    @@all.each { |dog| binding.pry }
+  end
+  
+end
