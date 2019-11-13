97991b3a5ca7a21894e083744b43193fed89bf27
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-13 00:33:34 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
-# Add your code here
+require 'pry'
+
+class Dog
+  
+  @@all = []
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @@all = save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all = []
+  end
+  
+  def self.print_all
+    @@all.each { |dog| puts dog.name }
+  end
+  
+  def save
+    @@all << self
+  end
+  
+end
